10b2f3370e683fe4bb4f20357e70a905522ccfc1
Neto test <viktoria.babatova@yandex.ru>
2023-04-20 12:35:01 +0300

    check ci

---
+++ src/test/java/MyTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MyTest {
+    @Test
+
+    public void shouldLessThanMaximumStation(){
+
+        Assertions.assertEquals(1, 1);
+
+    }
+}
